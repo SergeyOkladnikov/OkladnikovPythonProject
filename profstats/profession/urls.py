7e41ae2aa639c3a5f5b1856a297f2189a89f0bec
SergeyOkladnikov <mr.okladnicoff@yandex.ru>
2022-12-30 17:18:01 +0500

    +Маршрутизация

--- profstats/profession/urls.py
+++ profstats/profession/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('demand/', demand, name='demand'),
+    path('geography/', geography, name='geography'),
+    path('skills/', skills, name='skills'),
+    path('last-vacancies/', last_vacancies, name='last-vacancies')
 ]
--- profstats/profession/views.py
+++ profstats/profession/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse('Главная')
+
+
+def demand(request):
+    return HttpResponse('Востребованность')
+
+
+def geography(request):
+    return HttpResponse('География')
+
+
+def skills(request):
+    return HttpResponse('Навыки')
+
+
+def last_vacancies(request):
+    return HttpResponse('Последние вакансии')
